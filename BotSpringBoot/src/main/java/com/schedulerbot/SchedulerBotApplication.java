0f2734e9855f31e545b4a02554078de218f226ec
cay <cay@zipcodes-MBP-9.fios-router.home>
2021-12-21 17:12:09 -0500

    initial commit, adding spring boot

---
+++ BotSpringBoot/src/main/java/com/schedulerbot/SchedulerBotApplication.java
@@ -0,0 +1,13 @@
+package com.schedulerbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulerBotApplication.class, args);
+	}
+
+}
---
+++ BotSpringBoot/src/test/java/com/schedulerbot/SchedulerBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schedulerbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
