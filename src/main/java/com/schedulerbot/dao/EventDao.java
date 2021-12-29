5b0f14828b124806a62d8a3357f5ddc156791038
cay <cay@zipcodes-MBP-9.fios-router.home>
2021-12-29 15:34:33 -0500

    adding concretedao class

--- src/main/java/com/schedulerbot/dao/EventDao.java
+++ src/main/java/com/schedulerbot/dao/EventDao.java
@@ -2,16 +2,34 @@ package com.schedulerbot.dao;
 
 import com.schedulerbot.models.Event;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
 
 public class EventDao implements Dao<Event> {
+    Connection connection = ConnectionFactory.getConnection();
     @Override
     public Event getById(int id) {
-//        try{
-//            Statement statement = connection.createStatement();
-//        }
+        try{
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM event WHERE id=" + id);
+            if(resultSet.next()){
+                return eventFromResultSet(resultSet);
+            }
+        } catch (SQLException exception){
+            exception.printStackTrace();
+        }
         return null;
+    }
+
+    private Event eventFromResultSet(ResultSet resultSet) throws SQLException{
+        return new Event(
+                resultSet.getString(1),
+                resultSet.getDate(2),
+                resultSet.getTime(3),
+                resultSet.getString(4));
     }
 
     @Override
--- src/main/java/com/schedulerbot/models/Event.java
+++ src/main/java/com/schedulerbot/models/Event.java
@@ -1,13 +1,24 @@
 package com.schedulerbot.models;
 
+import org.springframework.cglib.core.Local;
+
+import java.sql.Date;
+import java.sql.Time;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class Event {
     String title;
-    LocalDate date;
-    LocalTime time;
+    Date date;
+    Time time;
     String description;
+
+    public Event(String title, Date date, Time time, String description) {
+        this.title = title;
+        this.date = date;
+        this.time = time;
+        this.description = description;
+    }
 
     public String getTitle() {
         return title;
@@ -17,19 +28,19 @@ public class Event {
         this.title = title;
     }
 
-    public LocalDate getDate() {
+    public Date getDate() {
         return date;
     }
 
-    public void setDate(LocalDate date) {
+    public void setDate(Date date) {
         this.date = date;
     }
 
-    public LocalTime getTime() {
+    public Time getTime() {
         return time;
     }
 
-    public void setTime(LocalTime time) {
+    public void setTime(Time time) {
         this.time = time;
     }
 
