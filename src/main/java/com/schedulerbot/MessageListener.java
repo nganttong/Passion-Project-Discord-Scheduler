50a914103fd5a3f4ebe84a2830f2b8fab5e99887
cay <cay@zipcodes-MBP-9.fios-router.home>
2021-12-30 18:03:57 -0500

    testing resposes

--- src/main/java/com/schedulerbot/MessageListener.java
+++ src/main/java/com/schedulerbot/MessageListener.java
@@ -1,6 +1,7 @@
 package com.schedulerbot;
 
 import discord4j.core.object.entity.Message;
+import discord4j.core.spec.MessageCreateMono;
 import reactor.core.publisher.Mono;
 
 public abstract class MessageListener {
@@ -8,11 +9,21 @@ public abstract class MessageListener {
     public Mono<Void> processCommand(Message eventMessage) {
         return Mono.just(eventMessage)
                 .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
-                .filter(message -> message.getContent().equalsIgnoreCase("!todo" || "!create"))
-                .flatMap(Message::getChannel)
-                .flatMap(channel -> channel.createMessage("Things to do today:\n - write a bot\n - eat lunch\n - play a game"))
+//                .filter(message -> message.getContent().equalsIgnoreCase("!todo"))
+                .flatMap(this::parseCommand)
+//                .flatMap(Message::getChannel)
+//                .flatMap(channel -> channel.createMessage("Things to do today:\n - write a bot\n - eat lunch\n - play a game"))
                 .then();
     }
 
+    public MessageCreateMono parseCommand(Message message){
+        String messageContent = message.getContent();
 
+        if (messageContent.equalsIgnoreCase("!todo")){
+            return message.getChannel().block().createMessage("test");
+        } else if (messageContent.equalsIgnoreCase("!create")){
+            return message.getChannel().block().createMessage("create");
+        }
+        return null;
+    }
 }
