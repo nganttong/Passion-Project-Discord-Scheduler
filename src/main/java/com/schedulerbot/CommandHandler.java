0923f064878ceacc14e466fe0cf5bceb6bac2827
cay <cay@zipcodes-MBP-9.fios-router.home>
2021-12-30 18:09:06 -0500

    added handler

---
+++ src/main/java/com/schedulerbot/CommandHandler.java
@@ -0,0 +1,5 @@
+package com.schedulerbot;
+
+public class CommandHandler {
+
+}
--- src/main/java/com/schedulerbot/MessageListener.java
+++ src/main/java/com/schedulerbot/MessageListener.java
@@ -4,6 +4,8 @@ import discord4j.core.object.entity.Message;
 import discord4j.core.spec.MessageCreateMono;
 import reactor.core.publisher.Mono;
 
+import java.util.Objects;
+
 public abstract class MessageListener {
 
     public Mono<Void> processCommand(Message eventMessage) {
@@ -11,6 +13,7 @@ public abstract class MessageListener {
                 .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
 //                .filter(message -> message.getContent().equalsIgnoreCase("!todo"))
                 .flatMap(this::parseCommand)
+                .filter(Objects::nonNull)
 //                .flatMap(Message::getChannel)
 //                .flatMap(channel -> channel.createMessage("Things to do today:\n - write a bot\n - eat lunch\n - play a game"))
                 .then();
