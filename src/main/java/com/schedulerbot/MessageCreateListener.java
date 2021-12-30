f92e03bfedeba11d8b1b758b5e6d8f8b81bb547d
cay <cay@zipcodes-MBP-9.fios-router.home>
2021-12-30 17:47:50 -0500

    testing multiple commands

--- src/main/java/com/schedulerbot/MessageCreateListener.java
+++ src/main/java/com/schedulerbot/MessageCreateListener.java
@@ -14,6 +14,6 @@ public class MessageCreateListener extends MessageListener implements EventListe
 
     @Override
     public Mono<Void> execute(MessageCreateEvent event) {
-        return processCreate(event.getMessage());
+        return processCommand(event.getMessage());
     }
 }
